03097720fa0b0e28201f15678d9921ec7f2ba351
almamarie <alouismariea97@gmail.com>
2022-01-17 00:01:52 +0000

    cropped the SLeM logo so it could fit and so ended up creating 2 more variants of the logo in the assets folder. Created the Welcome page but couldn't add the Welcome text.

--- lib/screens/home/welcome_screen.dart
+++ lib/screens/home/welcome_screen.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Welcome'),
+    return Scaffold(
+      body: SafeArea(
+          child: ListView(
+        children: [
+          Column(
+            children: [
+              SizedBox(
+                height: 90.h,
+              ),
+              Image.asset('assets/backgrounds/slem logo cropped 1.png'),
+              Text(
+                'SLEM',
+                style: TextStyle(fontSize: 33.sp),
+              ),
+              SizedBox(
+                height: 20.h,
+              ),
+              Image.asset('assets/backgrounds/background.png'),
+            ],
+          ),
+        ],
+      )),
     );
   }
 }
